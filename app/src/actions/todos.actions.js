3976a72f984d54e6865dc520086c0e9bfae4b48f
Yuval Allweil <themarquisdesheric@gmail.com>
2017-06-07 11:49:29 -0700

    created actions and constants

--- app/src/actions/todos.actions.js
+++ app/src/actions/todos.actions.js
@@ -1,1 +1,22 @@
+import * as actions from '../constants/todos.constants';
 
+export function addTodo(payload) {
+  return {
+    type: actions.ADD_TODO,
+    payload
+  };
+}
+
+export function removeTodo(payload) {
+  return {
+    type: actions.REMOVE_TODO,
+    payload
+  };
+}
+
+export function toggleTodo(payload) {
+  return {
+    type: actions.TOGGLE_TODO,
+    payload
+  };
+}
