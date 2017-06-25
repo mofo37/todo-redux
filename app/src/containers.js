38efecf4bf1041bb44b95c71db02b0a686e30303
Morgan Fogarty <mofo37@gmail.com>
2017-06-24 17:47:59 -0700

    add TodoContainer function to containers.

---
+++ app/src/containers.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from './actions';
+import Todo from './Todo';
+
+const mapStateToProps = (state) => state;
+
+function mapDispatchtoProps (dispatch) {
+  return bindActionCreators(actions, dispatch);
+}
+
+const TodoContainer = connect(mapStateToProps, mapDispatchtoProps)(Todo);
+
+export default TodoContainer;
