fffec7bbe7232575d13c03b4a60bc42f90af382e
Morgan Fogarty <mofo37@gmail.com>
2017-06-24 20:36:23 -0700

    finally rendering to page. Items added via Todo form. All items deleting.

--- app/src/components/Todo.js
+++ app/src/components/Todo.js
@@ -3,22 +3,26 @@ import ListItem from './ListItem'
 
 export default function Todo(props) {
 
-const { addTodo, removeTodo, toggleTodo, todos } = props;
-  
+  const { addTodo, removeTodo, toggleTodo, todos } = props;
+
   let input;
 
-  return(
+  return (
     <div>
       <ul>
         {todos.map((item, i) => {
-         return <ListItem key={i} id={item.id} name={item.name} toggleTodo={toggleTodo} completed={item.completed} removeTodo={removeTodo}/>;
-         })}
-        </ul>
+          return <ListItem key={i} id={item.id} name={item.name} toggleTodo={toggleTodo} completed={item.completed} removeTodo={removeTodo} />;
+        })}
+      </ul>
       <form onSubmit={(e) => {
         e.preventDefault();
         addTodo(input.value);
         input.value = '';
-        }}></form>
+      }}>
+        <label>What do you need to do?<input ref={node => {
+          input = node;
+        }} /></label>
+      </form>
     </div>
   );
 }
